00c6c5f671dc8eb285c2c4e1dee503614905bcbc
Aravind Pamoori <aravind.pamuri@gmail.com>
2022-03-03 11:59:28 -0600

    Add name.

--- webproject-exgit/src/main/java/HelloWorld.java
+++ webproject-exgit/src/main/java/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello World! \n");
+		response.getWriter().append("Hello World!! \n\n\n").append("Aravind Pamoori");
 		
 	}
 
